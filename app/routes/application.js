8ae88f1e51097839a9234742d26462b7aa9ae07f
Zoltan <zdebre@gmail.com>
2016-10-17 22:07:00 +1300

    Render dialog with controller

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,4 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  actions: {
+    showDialog() {
+      this.render('dialog', {
+        outlet: 'modal',
+        into: 'application',
+        controller: 'dialog'
+      });
+    }
+  }
 });
