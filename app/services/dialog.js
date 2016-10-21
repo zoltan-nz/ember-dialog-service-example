4d02b0f74f31f3eabb986aaa74906752977e7482
Zoltan <zdebre@gmail.com>
2016-10-21 22:07:00 +1300

    Dialog open close with button or with back

--- app/services/dialog.js
+++ app/services/dialog.js
@@ -1,4 +1,42 @@
 import Ember from 'ember';
 
-export default Ember.Service.extend({
+const {
+  Service,
+  computed
+} = Ember;
+
+export default Service.extend({
+
+  applicationRoute: computed('', function() {
+    return Ember.getOwner(this).lookup('route:application');
+  }),
+
+  applicationController: computed('', function() {
+    return Ember.getOwner(this).lookup('controller:application');
+  }),
+
+  open(template, model) {
+    this.get('applicationRoute').render(template, {
+      outlet: 'modal',
+      into: 'application',
+      controller: template,
+      model
+    });
+
+    const appCont = this.get('applicationController');
+    appCont.set('showDialog', true);
+    appCont.addObserver('showDialog', () => { this.close(); });
+  },
+
+  close() {
+    console.log('close called');
+    this.get('applicationRoute').disconnectOutlet({
+      outlet: 'modal',
+      parentView: 'application'
+    });
+
+    const appCont = this.get('applicationController');
+    appCont.removeObserver('showDialog');
+    appCont.set('showDialog', false);
+  }
 });
